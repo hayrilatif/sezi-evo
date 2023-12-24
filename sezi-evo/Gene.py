871cc0c000d1bbfd3c8efa58f38828b9e8f0fcf8
hayrilatifyilmaz <51344645+hayrilatifyilmaz@users.noreply.github.com>
2023-12-24 22:48:22 +0300

    Commit 4

--- sezi-evo/Gene.py
+++ sezi-evo/Gene.py
@@ -1,8 +1,10 @@
 class Gene():
-    def __init__(self,value):
+    def __init__(self,represantative_text:str,value):
+        self.__represantative_text=represantative_text
         self.__gene_values=value
     
     def get_gene(self):
         return self.__gene_values
     
-    
+    def get_representative_text(self)->str:
+        return self.__represantative_text
--- sezi-evo/Genome.py
+++ sezi-evo/Genome.py
@@ -5,8 +5,18 @@ from Gene import Gene
 
 class Genome():
     def __init__(self,iterable:Iterable(Gene)):
-        self.__gene_list={}
-        for index,gene in enumerate(iterable):self.__gene_list[f"gene-{index}"]=(gene)
-
+        self.__gene_dict={}
+        for index,gene in enumerate(iterable):self.__gene_dict[f"gene-{index}"]=(gene)
     def __repr__(self) -> str:
-        for gene 
+        for gene_key in self.__gene_dict.keys():
+            gene=self.__gene_dict.get(gene_key)
+            
+            print("GeneTypeObject")
+            print("*")
+            print(f"***{gene_key}***")
+            print("*")
+            print(f"Representative: {gene.get_representative_text()}")
+            print("*")
+            print(f"Values: {gene.get_gene()}")
+
+    
